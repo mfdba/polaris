4466d2b469fdc662b03ad94a775786025fc3be46
Antoine Gersant <antoine.gersant@lesforges.org>
2020-12-11 02:22:30 -0800

    async config::Manager

--- src/app/config/manager.rs
+++ src/app/config/manager.rs
@@ -1,7 +1,7 @@
 use diesel;
 use diesel::prelude::*;
-use std::time::Duration;
 use regex::Regex;
+use std::time::Duration;
 
 use super::*;
 use crate::app::user;
@@ -18,24 +18,24 @@ impl Manager {
 		Self { db, user_manager }
 	}
 
-	pub fn get_auth_secret(&self) -> Result<Vec<u8>, Error> {
+	pub async fn get_auth_secret(&self) -> Result<Vec<u8>, Error> {
 		use self::misc_settings::dsl::*;
-		let connection = self.db.connect()?;
+		let connection = self.db.connect().await?;
 		misc_settings
 			.select(auth_secret)
-			.get_result(&connection)
+			.get_result(&*connection)
 			.map_err(|e| match e {
 				diesel::result::Error::NotFound => Error::AuthSecretNotFound,
 				_ => Error::Unspecified,
 			})
 	}
 
-	pub fn get_index_sleep_duration(&self) -> Result<Duration, Error> {
+	pub async fn get_index_sleep_duration(&self) -> Result<Duration, Error> {
 		use self::misc_settings::dsl::*;
-		let connection = self.db.connect()?;
+		let connection = self.db.connect().await?;
 		misc_settings
 			.select(index_sleep_duration_seconds)
-			.get_result(&connection)
+			.get_result(&*connection)
 			.map_err(|e| match e {
 				diesel::result::Error::NotFound => Error::IndexSleepDurationNotFound,
 				_ => Error::Unspecified,
@@ -43,12 +43,12 @@ impl Manager {
 			.map(|s: i32| Duration::from_secs(s as u64))
 	}
 
-	pub fn get_index_album_art_pattern(&self) -> Result<Regex, Error> {
+	pub async fn get_index_album_art_pattern(&self) -> Result<Regex, Error> {
 		use self::misc_settings::dsl::*;
-		let connection = self.db.connect()?;
+		let connection = self.db.connect().await?;
 		misc_settings
 			.select(index_album_art_pattern)
-			.get_result(&connection)
+			.get_result(&*connection)
 			.map_err(|e| match e {
 				diesel::result::Error::NotFound => Error::IndexAlbumArtPatternNotFound,
 				_ => Error::Unspecified,
@@ -56,11 +56,11 @@ impl Manager {
 			.and_then(|s: String| Regex::new(&s).map_err(|_| Error::IndexAlbumArtPatternInvalid))
 	}
 
-	pub fn read(&self) -> anyhow::Result<Config> {
+	pub async fn read(&self) -> anyhow::Result<Config> {
 		use self::ddns_config::dsl::*;
 		use self::misc_settings::dsl::*;
 
-		let connection = self.db.connect()?;
+		let connection = self.db.connect().await?;
 
 		let mut config = Config {
 			album_art_pattern: None,
@@ -72,7 +72,7 @@ impl Manager {
 
 		let (art_pattern, sleep_duration) = misc_settings
 			.select((index_album_art_pattern, index_sleep_duration_seconds))
-			.get_result(&connection)?;
+			.get_result(&*connection)?;
 
 		config.album_art_pattern = Some(art_pattern);
 		config.reindex_every_n_seconds = Some(sleep_duration);
@@ -82,13 +82,13 @@ impl Manager {
 			use self::mount_points::dsl::*;
 			mount_dirs = mount_points
 				.select((source, name))
-				.get_results(&connection)?;
+				.get_results(&*connection)?;
 			config.mount_dirs = Some(mount_dirs);
 		}
 
 		let found_users: Vec<(String, i32)> = users::table
 			.select((users::columns::name, users::columns::admin))
-			.get_results(&connection)?;
+			.get_results(&*connection)?;
 		config.users = Some(
 			found_users
 				.into_iter()
@@ -102,25 +102,25 @@ impl Manager {
 
 		let ydns = ddns_config
 			.select((host, username, password))
-			.get_result(&connection)?;
+			.get_result(&*connection)?;
 		config.ydns = Some(ydns);
 
 		Ok(config)
 	}
 
-	pub fn amend(&self, new_config: &Config) -> anyhow::Result<()> {
-		let connection = self.db.connect()?;
+	pub async fn amend(&self, new_config: &Config) -> anyhow::Result<()> {
+		let connection = self.db.connect().await?;
 
 		if let Some(ref mount_dirs) = new_config.mount_dirs {
-			diesel::delete(mount_points::table).execute(&connection)?;
+			diesel::delete(mount_points::table).execute(&*connection)?;
 			diesel::insert_into(mount_points::table)
 				.values(mount_dirs)
-				.execute(&*connection)?; // TODO https://github.com/diesel-rs/diesel/issues/1822
+				.execute(&**connection)?; // TODO https://github.com/diesel-rs/diesel/issues/1822
 		}
 
 		if let Some(ref config_users) = new_config.users {
 			let old_usernames: Vec<String> =
-				users::table.select(users::name).get_results(&connection)?;
+				users::table.select(users::name).get_results(&*connection)?;
 
 			// Delete users that are not in new list
 			let delete_usernames: Vec<String> = old_usernames
@@ -129,7 +129,7 @@ impl Manager {
 				.filter(|old_name| config_users.iter().find(|u| &u.name == old_name).is_none())
 				.collect::<_>();
 			diesel::delete(users::table.filter(users::name.eq_any(&delete_usernames)))
-				.execute(&connection)?;
+				.execute(&*connection)?;
 
 			// Insert new users
 			let insert_users: Vec<&ConfigUser> = config_users
@@ -158,20 +158,20 @@ impl Manager {
 				// Update admin rights
 				diesel::update(users::table.filter(users::name.eq(&user.name)))
 					.set(users::admin.eq(user.admin as i32))
-					.execute(&connection)?;
+					.execute(&*connection)?;
 			}
 		}
 
 		if let Some(sleep_duration) = new_config.reindex_every_n_seconds {
 			diesel::update(misc_settings::table)
 				.set(misc_settings::index_sleep_duration_seconds.eq(sleep_duration as i32))
-				.execute(&connection)?;
+				.execute(&*connection)?;
 		}
 
 		if let Some(ref album_art_pattern) = new_config.album_art_pattern {
 			diesel::update(misc_settings::table)
 				.set(misc_settings::index_album_art_pattern.eq(album_art_pattern))
-				.execute(&connection)?;
+				.execute(&*connection)?;
 		}
 
 		if let Some(ref ydns) = new_config.ydns {
@@ -182,7 +182,7 @@ impl Manager {
 					username.eq(ydns.username.clone()),
 					password.eq(ydns.password.clone()),
 				))
-				.execute(&connection)?;
+				.execute(&*connection)?;
 		}
 
 		Ok(())
